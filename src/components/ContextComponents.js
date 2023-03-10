124caf6baa35a298006c7330447026a8da9107aa
SunTian <75829561+ysygMhdxw@users.noreply.github.com>
2023-03-10 10:01:25 +0800

    init

--- src/components/ContextComponents.js
+++ src/components/ContextComponents.js
@@ -1,21 +1,21 @@
 import PropTypes from 'prop-types';
 import { ContextString } from '../utils/enums';
-import { CaseManagement } from './caseManagement';
+import { CaseLearning } from './caseLearning';
 
 
 export const ContextComponents = (props) => {
     if (props.contextString == ContextString.caseLearning) {
         return (
             <>
-                <CaseManagement />
+                <CaseLearning />
             </>
         )
     }
-
     return (
         <>
             context
-        </>)
+        </>
+    )
 
 }
 

b39249ef3254edbf3c4feec6a76a955ba6810132
SunTian <75829561+ysygMhdxw@users.noreply.github.com>
2023-03-10 09:50:25 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // 由于 antd 组件的默认文案是英文，所以需要修改为中文
 import 'antd/dist/reset.css';
 import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn';
-import IndexLayout from './components/IndexLayout';
 import './index.css';
+import { HomePage } from './pages/HomePage';
 
 
 dayjs.locale('zh-cn');
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<IndexLayout />}></Route>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<IndexLayout />}></Route>
+    //   </Routes>
+    // </BrowserRouter>
+    <>
+      <HomePage />
+    </>
 
   );
 };

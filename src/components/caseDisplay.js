22c567d04e70cecbdcd11bbb9096582abdbe9a6f
SunTian <75829561+ysygMhdxw@users.noreply.github.com>
2023-03-11 19:21:39 +0800

    little debug

--- src/components/caseDisplay.js
+++ src/components/caseDisplay.js
@@ -2,6 +2,7 @@ import { Descriptions, Image } from 'antd';
 import propTypes from 'prop-types';
 import { useEffect } from 'react';
 export const CaseDisplay = (props) => {
+    const caseInfo = props.caseInfo
     useEffect(() => {
 
     }, [props]);
@@ -15,58 +16,58 @@ export const CaseDisplay = (props) => {
 
                 }}
             >
-                <Descriptions.Item label="病例编号">{props.caseInfo.case_number}</Descriptions.Item>
-                <Descriptions.Item label="病种名称">{props.caseInfo.disease_type}</Descriptions.Item>
-                <Descriptions.Item label="宠物名称">{props.caseInfo.pet_name}</Descriptions.Item>
-                <Descriptions.Item label="宠物种类">{props.caseInfo.pet_species}</Descriptions.Item>
-                <Descriptions.Item label="宠物年龄">{props.caseInfo.pet_age}</Descriptions.Item>
-                <Descriptions.Item label="宠物主人姓名">{props.caseInfo.owner_name}</Descriptions.Item>
-                <Descriptions.Item label="宠物主人电话">{props.caseInfo.owner_phone}</Descriptions.Item>
-                <Descriptions.Item label="治疗费用">{props.caseInfo.expense}</Descriptions.Item>
+                <Descriptions.Item label="病例编号">{caseInfo.case_number}</Descriptions.Item>
+                <Descriptions.Item label="病种名称">{caseInfo.disease_type}</Descriptions.Item>
+                <Descriptions.Item label="宠物名称">{caseInfo.pet_name}</Descriptions.Item>
+                <Descriptions.Item label="宠物种类">{caseInfo.pet_species}</Descriptions.Item>
+                <Descriptions.Item label="宠物年龄">{caseInfo.pet_age}</Descriptions.Item>
+                <Descriptions.Item label="宠物主人姓名">{caseInfo.owner_name}</Descriptions.Item>
+                <Descriptions.Item label="宠物主人电话">{caseInfo.owner_phone}</Descriptions.Item>
+                <Descriptions.Item label="治疗费用">{caseInfo.expense}</Descriptions.Item>
 
 
                 <Descriptions.Item label="主要病症">
                     <div>
-                        症状描述：{props.caseInfo.symptom.text}
+                        症状描述：{caseInfo.symptom.text}
                         <br />
                         <Image
                             width={200}
                             height={200}
                             src="error"
-                            fallback={props.caseInfo.symptom.pic}
+                            fallback={caseInfo.symptom.pic}
                         />
                         <br />
-                        视频url：{props.caseInfo.symptom.video}
+                        视频url：{caseInfo.symptom.video}
                         <br />
                     </div>
                 </Descriptions.Item>
                 <Descriptions.Item label="诊断结果">
                     <div>
-                        诊断结果：{props.caseInfo.diagnosis_result.text}
+                        诊断结果：{caseInfo.diagnosis_result.text}
                         <br />
                         <Image
                             width={200}
                             height={200}
                             src="error"
-                            fallback={props.caseInfo.diagnosis_result.pic}
+                            fallback={caseInfo.diagnosis_result.pic}
                         />
                         <br />
-                        视频url：{props.caseInfo.diagnosis_result.video}
+                        视频url：{caseInfo.diagnosis_result.video}
                         <br />
                     </div>
                 </Descriptions.Item>
                 <Descriptions.Item label="治疗方案">
                     <div>
-                        治疗方案：{props.caseInfo.treatment.text}
+                        治疗方案：{caseInfo.treatment.text}
                         <br />
                         <Image
                             width={200}
                             height={200}
                             src="error"
-                            fallback={props.caseInfo.treatment.pic}
+                            fallback={caseInfo.treatment.pic}
                         />
                         <br />
-                        视频url：{props.caseInfo.treatment.video}
+                        视频url：{caseInfo.treatment.video}
                         <br />
                     </div>
                 </Descriptions.Item>

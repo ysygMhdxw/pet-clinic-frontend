07966d9ed1c479a951b6da6d58c732826aa20e4f
悄兮 <2509253965@qq.com>
2023-04-16 17:46:40 +0800

    Update quizManagement.js

--- src/components/caseTestManagement/quizManagement.js
+++ src/components/caseTestManagement/quizManagement.js
@@ -1,5 +1,6 @@
-import { ModalForm } from '@ant-design/pro-components';
-import { Table, Space, Button, message, Form, Input, InputNumber } from 'antd';
+/* eslint-disable no-unused-vars */
+import {  StepsForm } from '@ant-design/pro-components';
+import { Table, Space, Button, message, Form, Input, InputNumber ,Modal,Transfer} from 'antd';
 import React from 'react';
 import { useEffect , useState } from 'react'
 import {PlusOutlined} from "@ant-design/icons"
@@ -51,6 +52,7 @@ export const QuizManagement = (  ) => {
           ),
         },
       ];
+    
 
       const waitTime = (time = 100) => {
         return new Promise((resolve) => {
@@ -69,56 +71,194 @@ export const QuizManagement = (  ) => {
 
       useEffect(()=>{
         getQuizListData();
-        
-  
     },[]);
-
-      const onChange = (pagination, filters, sorter, extra) => {
-      console.log('params', pagination, filters, sorter, extra);
+    
+     const [isModalOpen, setIsModalOpen] = useState(false)
+     const onChange = (pagination, filters, sorter, extra) => {
+     console.log('params', pagination, filters, sorter, extra);
       };
+    
+      //students 穿梭框
+     const [studentsData, setStudentsData] = useState([]);
+     const [targetKeys, setTargetKeys] = useState([]);
+     const filterOption = (inputValue, option) => option.key.indexOf(inputValue) > -1;
+     const handleChange = (newTargetKeys) => {
+        setTargetKeys(newTargetKeys);
+      };
+      const handleSearch = (dir, value) => {
+        //console.log('search:', dir, value);
+      };
+      useEffect(() => {
+        getStudentsData();
+      }, []);
+    
+      async function getStudentsData () {
+        const tempTargetKeys = [];
+        const tempData = [];
+        const res = await api.getUsers()
+        const data = res.data
+        console.log(data.users);
+        data.users.forEach(user => {
+            const stu = {
+                key: user.id,
+                title: user.username,
+              };
+              if (stu.chosen) {
+                tempTargetKeys.push(stu.key);
+              }
+              tempData.push(stu);
+        });
+        setStudentsData(tempData);
+        setTargetKeys(tempTargetKeys);
+      }
+
+
+      //添加题目
+      const[singleNum,setSingleNum] =useState();
+      const [multiNum,setMultiNum] =useState();
+      const [tofNum,setTofNum] =useState();
+      const [textNum,setTextNum] =useState();
+
     return (
         <>
-         <ModalForm
-            labelWidth="auto"
-            trigger={
-                <Button type="primary">
-                 <PlusOutlined/>
-                  新建考试
-                </Button>}
-            //form={form}
-            onFinish={async (values) => {
-                            await waitTime(1000);
-                            console.log(values);
-                            //addQuestion(values);
-                            //getQuestionData();
-                            message.success('新建成功');
-                            return true;
-            }}
-         >
-            <Form.Item
-            name="name"
-            label="考试名称"
-            rules={[{ required: true, message: '请输入考试名称' }]}
+         <Button type="primary" onClick={()=>{setIsModalOpen(true)}}>
+            <PlusOutlined/>
+             新建考试
+         </Button>
+         <StepsForm
+        onFinish={async (values) => {
+          let resp = {
+            name: values.name,
+            description: values.description,
+            duration: values.duration,
+            students: {list:targetKeys},
+            questions:
+            {
+                single:[],
+                multi: [],
+                tof: [],
+                text:[]
+            }
+          }
+          console.log(resp)
+          console.log(singleNum)
+          //console.log(values);
+          //console.log(targetKeys)
+          await waitTime(1000);
+          setIsModalOpen(false);
+          message.success('提交成功');
+        }}
+        formProps={{
+          validateMessages: {
+            required: '此项为必填项',
+          },
+        }}
+        stepsFormRender={(dom, submitter) => {
+          return (
+            <Modal
+              title="新建考试"
+              width={800}
+              onCancel={() => setIsModalOpen(false)}
+              open={isModalOpen}
+              footer={submitter}
+              destroyOnClose
+            >
+              {dom}
+            </Modal>
+          );
+        }}
+      >
+        <StepsForm.StepForm
+          name="base"
+          title="创建考试"
+          onFinish={async () => {
+            await waitTime(500);
+            return true;
+          }}
         >
-            <Input />
-            </Form.Item>
-            <Form.Item
-          name="description"
-          label="考试描述"
-          rules={[{ required: true, message: '请输入考试描述' }]}
-        >
-          <Input.TextArea />
-        </Form.Item>
+              <Form.Item
+                name="name"
+                label="考试名称"
+                rules={[{ required: true, message: '请输入考试名称' }]}
+              >
+                <Input />
+              </Form.Item>
+    
+              <Form.Item
+                name="description"
+                label="考试描述"
+                rules={[{ required: true, message: '请输入考试描述' }]}
+              >
+                <Input.TextArea />
+              </Form.Item>
+    
+              <Form.Item
+                name="duration"
+                label="用时"
+                rules={[{ required: true, message: '请输入用时' }]}
+              >
+                <InputNumber />
+              </Form.Item>
+         
+        </StepsForm.StepForm>
 
+        <StepsForm.StepForm name="question" title="添加试题"  
+        onFinish={async () => {
+            await waitTime(500);
+            return true;
+          }}>
         <Form.Item
-          name="duration"
-          label="用时"
-          rules={[{ required: true, message: '请输入用时' }]}
-        >
-          <InputNumber />
-        </Form.Item>
+                name="single"
+                label="单选题"
+              >
+                <InputNumber
+                defaultValue={0}
+                onChange={(value)=>{setSingleNum(value)}} />  道
+              </Form.Item>
 
-         </ModalForm>
+              <Form.Item
+                name="multi"
+                label="多选题"
+              >
+                <InputNumber
+                defaultValue={0}
+                onChange={(value)=>{setMultiNum(value)}} /> 道
+              </Form.Item>
+
+              <Form.Item
+                name="tof"
+                label="判断题"
+              >
+                <InputNumber
+                defaultValue={0}
+                onChange={(value)=>{setTofNum(value)}} /> 道
+              </Form.Item>
+
+              <Form.Item
+                name="text"
+                label="问答题"
+              >
+                <InputNumber
+                defaultValue={0}
+                onChange={(value)=>{setTextNum(value)}} /> 道
+              </Form.Item>
+        </StepsForm.StepForm>  
+        <StepsForm.StepForm name="students" title="添加学生">
+        <Form.Item>
+        <Transfer
+         dataSource={studentsData}
+         showSearch
+         filterOption={filterOption}
+         targetKeys={targetKeys}
+         onChange={handleChange}
+         onSearch={handleSearch}
+         render={(item) => item.title}
+        />
+        </Form.Item>
+        </StepsForm.StepForm>
+
+ 
+      </StepsForm>
 
 
         <Table columns={columns} dataSource={quizListData} onChange={onChange} />

dc7e32788e6e4747ee6521e9ac7b4548f2854566
SunTian <75829561+ysygMhdxw@users.noreply.github.com>
2023-04-20 13:50:46 +0800

    edit the video could not close problem

--- src/components/caseStudy/videoModal.js
+++ src/components/caseStudy/videoModal.js
@@ -3,25 +3,41 @@ import {Button, Modal} from "antd";
 import {VideoCameraOutlined} from "@ant-design/icons";
 import ReactPlayer from "react-player";
 import PropTypes from "prop-types";
-
-export const VideoModal = ({ videoUrl }) => {
+const waitTime = (time = 100) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(true);
+        }, time);
+    });
+};
+export const VideoModal = ({videoUrl}) => {
     const [visible, setVisible] = useState(false);
+    const [playing, setPlaying] = useState(false);
+
+    const handleCancel = async () => {
+        // 停止视频播放
+        setPlaying(false);
+        await waitTime(200);
+        setVisible(false);
+    };
 
     const showModal = () => {
         setVisible(true);
     };
 
-    const handleCancel = () => {
-        setVisible(false);
-    };
-
     return (
-        <div >
-            <Button type="primary" onClick={showModal} icon={<VideoCameraOutlined />}>
+        <div>
+            <Button type="primary" onClick={showModal} icon={<VideoCameraOutlined/>}>
                 点击观看视频
             </Button>
             <Modal title="视频" visible={visible} onCancel={handleCancel} footer={null}>
-                <ReactPlayer url={videoUrl} controls width="100%" height="100%" />
+                <ReactPlayer
+                    playing={playing} // 控制视频播放状态
+                    onPlay={() => setPlaying(true)} // 更新播放状态
+                    onPause={() => setPlaying(false)} // 更新播放状态
+                    url={videoUrl}
+                    controls
+                    width="100%" height="100%"/>
             </Modal>
         </div>
     );
